c5ae1a0065c2c8bd3c3e6284569a073aef15f59b
Orçun Çolak <orcun.colak@hazelcast.com>
2024-02-08 08:17:22 +0300

    Initial commit

---
+++ itenary/src/main/java/com/colak/itenary/ItenaryApplication.java
@@ -0,0 +1,12 @@
+package com.colak.itenary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItenaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItenaryApplication.class, args);
+    }
+}
